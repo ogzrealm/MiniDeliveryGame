5515980fd584db081e56d3027382c8377a68dc60
Oğuzhan ARSLAN <coffeeandnulls@gmail.com>
2026-02-21 05:44:04 +0300

    First Commit

---
+++ Assets/Development/CarControlScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CarControlScript : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float turnSpeed;
+    
+    public void Moving(float moveValue)
+    {
+        Vector3 direction = Vector3.up * moveValue;
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
+
+    public void Turning(float turnValue)
+    {
+        var turn = turnValue * turnSpeed;
+        transform.Rotate(0,0,turn*Time.deltaTime);
+    }
+}
---
+++ Assets/Development/InputManagerScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManagerScript : MonoBehaviour
+{
+    public CarControlScript car;
+    void Update()
+    {
+        if (Keyboard.current.wKey.isPressed)
+        {
+            car.Moving(1);
+        }
+
+        if (Keyboard.current.sKey.isPressed)
+        {
+            car.Moving(-1);
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            car.Turning(1);
+        }
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            car.Turning(-1);
+        }
+        
+    }
+}
