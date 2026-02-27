b2514126907d7425ac957ad2fa089adbdebec514
Oğuzhan ARSLAN <coffeeandnulls@gmail.com>
2026-02-28 02:59:20 +0300

    v0.1.0

--- Assets/Development/CarControlScript.cs
+++ Assets/Development/CarControlScript.cs
@@ -1,10 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CarControlScript : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float turnSpeed;
-    
+    private float origiinalSpeed;
+    private bool isSlowed = false;
+    private IEnumerator dumpRoutine;
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        origiinalSpeed = moveSpeed;
+    }
+
     public void Moving(float moveValue)
     {
         Vector3 direction = Vector3.up * moveValue;
@@ -15,7 +29,37 @@ public class CarControlScript : MonoBehaviour
     {
         var turn = turnValue * turnSpeed;
         transform.Rotate(0,0,turn*Time.deltaTime);
-        Debug.Log(turn);
+        
         
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("DumpTag"))
+        {
+            if (isSlowed && dumpRoutine != null)
+            {
+                StopCoroutine(dumpRoutine);
+            }
+            dumpRoutine = Slow();
+            StartCoroutine(dumpRoutine);
+        }
+    }
+
+    IEnumerator Slow()
+    {
+        moveSpeed=origiinalSpeed*0.5f;
+        isSlowed = true;
+        Debug.Log("Your Speed is Half");
+        
+        yield return new WaitForSeconds(1.5f);
+        
+        isSlowed = false;
+        moveSpeed = origiinalSpeed;
+        Debug.Log("Your Speed is Normal");
+        dumpRoutine = null;
+        
+    }
+    
+    
 }
